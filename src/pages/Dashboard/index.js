73231aa5b9fe278aa62cf8adde07659ba870289c
Diego Azevedo <dapptn@hotmail.com>
2020-01-30 23:31:31 +1100

    Provider schedule list

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,39 +1,87 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  setMilliseconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+
 const Dashboard = () => {
+  const [date, setDate] = useState(new Date());
+  const [schedule, setSchedule] = useState([]);
+
+  const dateFormatted = useMemo(() => format(date, "d 'of' MMMM"), [date]);
+
+  const handlePreviousDay = () => {
+    setDate(subDays(date, 1));
+  };
+
+  const handleNextDay = () => {
+    setDate(addDays(date, 1));
+  };
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule/provider', {
+        params: { date },
+      });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setMilliseconds(
+          setSeconds(setMinutes(setHours(date, hour), 0), 0),
+          0
+        );
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a =>
+            isEqual(parseISO(a.date), compareDate)
+          ),
+        };
+      });
+      setSchedule(data);
+    }
+    loadSchedule();
+  }, [date]);
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePreviousDay}>
           <MdChevronLeft size={36} color="#fff"></MdChevronLeft>
         </button>
-        <strong>31 of May</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#fff"></MdChevronRight>
         </button>
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Diego Azevedo</span>
-        </Time>
-        <Time available>
-          <strong>08:00</strong>
-          <span>Slot available</span>
-        </Time>
-        <Time>
-          <strong>08:00</strong>
-          <span>Diego Azevedo</span>
-        </Time>
-        <Time>
-          <strong>08:00</strong>
-          <span>Diego Azevedo</span>
-        </Time>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Available slot'}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  min-height: 680px;
+  overflow: auto;
   background: linear-gradient(180deg, #141e30, #243b55);
 `;
